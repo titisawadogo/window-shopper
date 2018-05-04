3297524a78443fab04cd582f4b0047a41ba62edc
Sawadogo Thierry <sawadogothierry@Sawadogos-MacBook-Pro.local>
2018-05-04 17:21:24 -0400

    first window shop

---
+++ window-shopper-app/window-shopper-app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper-app
+//
+//  Created by Sawadogo Thierry on 5/4/18.
+//  Copyright © 2018 Sawadogo Thierry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
